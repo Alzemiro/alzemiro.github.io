c7e6e5407b35a104044f656ffb6ec3277acba9d2
Alzemiro Iago de Moraes Thomaz <iagomoraest@gmail.com>
2024-06-19 17:17:49 -0300

    strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,26 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client'
 import "../index.css";
 import '@radix-ui/themes/styles.css';
 import "./i18nfy.js"
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {ApiProvider} from "./Context/apiprovider.jsx";
-import {HomePage} from "./pages/Home/home.page.jsx";
-import {AboutMe} from "./pages/AboutMe/aboutme.page.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ApiProvider } from "./Context/apiprovider.jsx";
+import { HomePage } from "./pages/Home/home.page.jsx";
+import { AboutMe } from "./pages/AboutMe/aboutme.page.jsx";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <ApiProvider value={<HomePage/>}/>,
+        element: <ApiProvider value={<HomePage />} />,
         children: [
             {
                 path: "/",
-                element: <HomePage/>
+                element: <HomePage />
             },
             {
                 path: "/about-me",
-                element: <AboutMe/>
+                element: <AboutMe />
             }],
 
     },
@@ -28,5 +29,7 @@ const router = createBrowserRouter([
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router}/>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
