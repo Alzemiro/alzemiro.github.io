e971cb0abe086c703dc56048db7cff52060537f9
Alzemiro Iago de Moraes Thomaz <iagomoraest@gmail.com>
2024-06-19 18:05:51 -0300

    ajustes finais

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client'
 import "../index.css";
 import '@radix-ui/themes/styles.css';
 import "./i18nfy.js"
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createHashRouter, RouterProvider, useLocation } from "react-router-dom";
 import { ApiProvider } from "./Context/apiprovider.jsx";
 import { HomePage } from "./pages/Home/home.page.jsx";
 import { AboutMe } from "./pages/AboutMe/aboutme.page.jsx";
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <ApiProvider/>,
@@ -26,10 +26,22 @@ const router = createBrowserRouter([
     },
 ]);
 
+function RemoveHash() {
+  const location = useLocation();
 
+  useEffect(() => {
+    const path = location.pathname + location.search;
+    if (path !== '/') { // Evita loop infinito na página inicial
+      window.history.pushState(null, null, path);
+    }
+  }, [location]);
+
+  return null;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <RouterProvider router={router} />
+        <RemoveHash />
     </React.StrictMode>
 )
